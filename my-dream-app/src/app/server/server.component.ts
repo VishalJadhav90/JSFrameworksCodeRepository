babf91854bdfe68c7e8bf23f48ad7b5fc71afcfb
VishalJadhav90 <jadhav.vsh@gmail.com>
2020-04-18 11:12:09 +0530

    directives and much more about angular.

--- my-dream-app/src/app/server/server.component.ts
+++ my-dream-app/src/app/server/server.component.ts
@@ -5,5 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: "./server.component.html"
 })
 export class ServerComponent {
+  private serverId: number = 10;
+  private serverStatus: string = '';
 
+  constructor() {
+    if (Math.random()>0.5) {
+      this.serverStatus = 'Online';
+    } else {
+      this.serverStatus = 'Offline';
+    }
+  }
+
+  getServerId() {
+    return this.serverId;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    if (this.serverStatus === "Online") {
+      return 'green';
+    } else {
+      return 'red';
+    }
+  }
 }
--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -4,16 +4,34 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', //select by html element
   //selector: '[app-servers]', //select by html attribute
   //selector: ".app-servers", //select by class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  newServerEnabled = false;
+  serverStatus = "server is offline";
+  serverName = "";
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.newServerEnabled = true;
+      this.showServerStatus();
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  showServerStatus() {
+    if (this.newServerEnabled) {
+      this.serverStatus = "Server is online";
+    } else {
+      this.serverStatus = "Server is offline";
+    }
+  }
+
+  setServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
