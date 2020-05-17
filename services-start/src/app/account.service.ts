ca7afe5d72a61f58c8de05dbe5e56cc2cc55dab5
VishalJadhav90 <jadhav.vsh@gmail.com>
2020-05-17 22:12:16 +0530

    Added code for services

---
+++ services-start/src/app/account.service.ts
@@ -0,0 +1,33 @@
+import { LoggingService } from "./logging.service";
+import { EventEmitter } from "@angular/core";
+
+export class AccountService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  statusUpdated: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private logginService: LoggingService) {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name: name, status: status});
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+    this.logginService.logStatusChange(status);
+
+  }
+}
